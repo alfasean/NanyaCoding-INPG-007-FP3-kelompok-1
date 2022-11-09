73a23135364e7fa780cbaac5be8f84bbfd469f7b
Your Name <you@example.com>
2022-11-10 00:20:51 +0800

    set responsive design

---
+++ js/script.js
@@ -0,0 +1,41 @@
+var backToTop = document.querySelector(".back-to-top");
+
+window.addEventListener("scroll", () => {
+  if (this.scrollY >= 450) {
+    backToTop.classList.add("show");
+
+    backToTop.addEventListener("click", () => {
+      window.scrollTo({ top: 0 });
+    });
+  } else {
+    backToTop.classList.remove("show");
+  }
+});
+
+const productContainers = [...document.querySelectorAll('.container-services')];
+const nxtBtn = [...document.querySelectorAll('.nxt-btn')];
+const preBtn = [...document.querySelectorAll('.pre-btn')];
+
+productContainers.forEach((item, i) => {
+    let containerDimensions = item.getBoundingClientRect();
+    let containerWidth = containerDimensions.width;
+
+    nxtBtn[i].addEventListener('click', () => {
+        item.scrollLeft += containerWidth;
+    })
+
+    preBtn[i].addEventListener('click', () => {
+        item.scrollLeft -= containerWidth;
+    })
+})
+
+const viewBtn = document.querySelector(".popup_button"),
+popup = document.querySelector(".popup"),
+close = popup.querySelector(".close")
+
+viewBtn.onclick = ()=>{
+  popup.classList.toggle("show");
+}
+close.onclick = ()=>{
+  viewBtn.click();
+}
