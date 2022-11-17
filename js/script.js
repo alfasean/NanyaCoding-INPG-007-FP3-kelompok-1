d23ba8fe6d114dca5db82a649aeeb92298023cf6
Your Name <you@example.com>
2022-11-18 02:45:00 +0800

    modified element

--- js/script.js
+++ js/script.js
@@ -45,3 +45,17 @@ viewBtn.onclick = () => {
 close.onclick = () => {
   viewBtn.click();
 }
+
+const btn = document.querySelector(".btn_rate");
+const post = document.querySelector(".post");
+const widget = document.querySelector(".star-widget");
+const editBtn = document.querySelector(".edit");
+btn.onclick = () => {
+  widget.style.display = "none";
+  post.style.display = "block";
+  editBtn.onclick = () => {
+    widget.style.display = "block";
+    post.style.display = "none";
+  }
+  return false;
+}
